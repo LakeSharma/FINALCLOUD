fb97dfb8a0e20aef446d9fc26d1d93cd7e699315
LakeSharma <77363127+LakeSharma@users.noreply.github.com>
2022-05-22 16:50:33 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,115 @@
+import "./App.css";
+import { useMemo } from "react";
+
+import Minter from "./Minter";
+
+import * as anchor from "@project-serum/anchor";
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  getPhantomWallet,
+  getSolflareWallet,
+  getSolletWallet,
+} from "@solana/wallet-adapter-wallets";
+
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+
+import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
+import { ThemeProvider, createTheme } from "@material-ui/core";
+
+
+const theme = createTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
+const candyMachineId = process.env.REACT_APP_CANDY_MACHINE_ID
+  ? new anchor.web3.PublicKey(process.env.REACT_APP_CANDY_MACHINE_ID)
+  : undefined;
+
+const network = process.env.REACT_APP_SOLANA_NETWORK as WalletAdapterNetwork;
+
+const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST!;
+const connection = new anchor.web3.Connection(rpcHost);
+
+const startDateSeed = parseInt(process.env.REACT_APP_CANDY_START_DATE!, 10);
+
+const txTimeout = 30000; // milliseconds (confirm this works for your project)
+
+const App = () => {
+  const endpoint = useMemo(() => clusterApiUrl(network), []);
+
+  const wallets = useMemo(
+    () => [getPhantomWallet(), getSolflareWallet(), getSolletWallet()],
+    []
+  );
+
+  function toggleMenu() {
+    const menu = document.getElementById("mobileNavContainer")!;
+    menu.classList.toggle("open-menu");
+    console.log("pressed");
+  }
+
+  return (
+    <div>
+      <div id="mobileNavContainer" className="mobile-nav">
+        <div className="mobile-nav-close-button" >
+          <img src="/icons/close.svg" alt="" onClick={toggleMenu}/>
+        </div>
+        <ul>
+          <li>
+            <img className="mobile-nav-logo" src="/img/logo.png" alt="" />
+          </li>
+          <li>
+            <div className="social-icons">
+              <img className="nav-social" src="/icons/twitter.svg" alt="" />
+              <img className="nav-social" src="/icons/discord.svg" alt="" />
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="mobile-menu-button" onClick={toggleMenu}>
+        <img src="/icons/menu.svg" alt="" />
+      </div>
+      <nav>
+        <div className="nav-container">
+          <img className="nav-logo" src="/img/logo.png" alt="" />
+          <div className="social-icons hide-800">
+            <img className="nav-social" src="/icons/twitter.svg" alt="" />
+            <img className="nav-social" src="/icons/discord.svg" alt="" />
+          </div>
+        </div>
+      </nav>
+      <div className="content-wrapper">
+          <header className="card" id="link1">
+            <div>
+              <ThemeProvider theme={theme}>
+                <ConnectionProvider endpoint={endpoint}>
+                  <WalletProvider wallets={wallets} autoConnect>
+                    <WalletDialogProvider>
+
+                        <Minter
+                          candyMachineId={candyMachineId}
+                          connection={connection}
+                          startDate={startDateSeed}
+                          txTimeout={txTimeout}
+                          rpcHost={rpcHost}
+                        />
+
+                    </WalletDialogProvider>
+                  </WalletProvider>
+                </ConnectionProvider>
+              </ThemeProvider>
+            </div>
+          </header>
+
+      </div>
+    </div>
+  );
+};
+
+export default App;
