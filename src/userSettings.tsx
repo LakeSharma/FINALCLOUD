c875c3e40b3bfd076e1a6c540227ba63bb476977
Austin <support@thetaproxies.com>
2022-07-12 00:30:14 -0400

    new cm

--- src/userSettings.tsx
+++ src/userSettings.tsx
@@ -138,7 +138,7 @@ export class MintWhitelistCustomHTML extends React.Component {
 export const publicSaleSettings: PublicSaleSettings = {
   //start date and end date must match your Candy Machine Config for public launch!
 
-  startDate: date("25 Dec 2021 00:00:00 GMT"),
+  startDate: date("11 July 2022 23:00:00 EST"),
   endDate: undefined,
   countdown: false,
   //Example date below
